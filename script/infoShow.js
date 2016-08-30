24d432c49ed468af2733b6682f4da84ff6fdc813
Dawid Kubic <dawid.kubic@gmail.com>
2016-08-30 10:02:30 +0200

    angular and new menu style

---
+++ script/infoShow.js
@@ -0,0 +1,70 @@
+var appCmd = angular.module('infoShowApp', []);
+
+appCmd.controller('menuController', function menuController($scope){
+
+    $scope.upLeftInfo = true;
+    $scope.upMiddleInfo = true;
+    $scope.upRightInfo = true;
+    $scope.middleLeftInfo = true;
+    $scope.middleRightInfo = true;
+    $scope.bottomLeftInfo = true;
+    $scope.bottomMiddleInfo = true;
+    $scope.bottomRightInfo = true;
+
+});
+
+appCmd.directive('up-left', function () {
+    return {
+        restrict: 'E',
+        templateUrl: html/upLeftInfo.html
+    }
+});
+
+appCmd.directive('up-middle', function () {
+    return {
+        restrict: 'E',
+        templateUrl: html/upMiddleInfo.html
+    }
+});
+
+appCmd.directive('up-right', function () {
+    return {
+        restrict: 'E',
+        templateUrl: html/upRightInfo.html
+    }
+});
+
+appCmd.directive('middle-left', function () {
+    return {
+        restrict: 'E',
+        templateUrl: html/middleLeftInfo.html
+    }
+});
+
+appCmd.directive('middle-right', function () {
+    return {
+        restrict: 'E',
+        templateUrl: html/upLeftInfo.html
+    }
+});
+
+appCmd.directive('bottom-left', function () {
+    return {
+        restrict: 'E',
+        templateUrl: html/upLeftInfo.html
+    }
+});
+
+appCmd.directive('bottom-middle', function () {
+    return {
+        restrict: 'E',
+        templateUrl: html/upLeftInfo.html
+    }
+});
+
+appCmd.directive('bottom-right', function () {
+    return {
+        restrict: 'E',
+        templateUrl: html/upLeftInfo.html
+    }
+});
